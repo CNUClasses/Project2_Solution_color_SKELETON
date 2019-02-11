e9a34373d2d2d28ad3326fc65b2d1aa3130a410e
Keith <kperkins411@gmail.com>
2019-02-11 00:16:43 -0500

    added runtime permissions, made target API 23

--- project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
+++ project2_Solution_color/src/main/java/com/example/solution_color/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.solution_color;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.library.bitmap_utilities.BitMap_Helpers;
 
 public class MainActivity extends AppCompatActivity {
 
-     @Override
+    //permission request stuff for 6.0 and above
+    private String[] perms = {"android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE"};
+    private static final int PERMS_REQ_CODE = 200;
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -22,7 +35,36 @@ public class MainActivity extends AppCompatActivity {
          getSupportActionBar().setDisplayShowHomeEnabled(false);
 
 
+        //if running on a version > 22 then need runtime permissions
+        if (Build.VERSION.SDK_INT>Build.VERSION_CODES.LOLLIPOP_MR1)
+            requestPermissions(perms, PERMS_REQ_CODE);
+        else
+            postPermissionSetup();
+
      }
+
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults) {
+        boolean writeAccepted = false;
+        boolean readAccepted = false;
+
+        switch (permsRequestCode) {
+            case PERMS_REQ_CODE:
+                writeAccepted = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                readAccepted = grantResults.length > 0 && grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                 break;
+        }
+
+        if (writeAccepted&& readAccepted)
+            postPermissionSetup();
+        else
+            Toast.makeText(this,"YOU DONT HAVE PROPER PERMISSIONS",Toast.LENGTH_SHORT).show();
+    }
+
+    //use this function if you want, I use it after permissions are organized
+    private void postPermissionSetup(){
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,8 +87,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
-
-
-
 }
 
